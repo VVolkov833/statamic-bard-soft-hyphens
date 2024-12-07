1d591f06767b033c557d832d095253426b911e7b
VVolkov833 <vadim.volkov.dev@gmail.com>
2024-12-07 19:55:16 +0100

    replace Bard class bind with hooks

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace Kabocom\SoftHyphens;
 
-use Statamic\Statamic; // ++-- clear these up
-use Illuminate\Support\Facades\View;
-use Statamic\Fieldtypes\Bard\Augmentor;
+use Statamic\Fieldtypes\Bard;
 use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
@@ -13,12 +11,34 @@ class ServiceProvider extends AddonServiceProvider
         __DIR__.'/../dist/js/soft-hyphens.js',
     ];
 
-    public function register() {
-        $this->app->bind(\Statamic\Fieldtypes\Bard::class, Bard::class);
+    public function boot()
+    {
+        parent::boot();
+
+        Bard::hook('process', function ($payload, $next) {
+            $payload = ServiceProvider::replaceSoftHyphens($payload, '↵', '­'); // Modify before saving
+            return $next($payload);
+        });
+
+        Bard::hook('pre-process', function ($payload, $next) {
+            $payload = ServiceProvider::replaceSoftHyphens($payload, '­', '↵'); // Modify before rendering in editor
+            return $next($payload);
+        });
     }
 
-    public function boot() {
-        parent::boot();
-        //Augmentor::addExtension('soft-hyphens', new Bard());
+    public static function replaceSoftHyphens($value, $search, $replace)
+    {
+        if (is_array($value)) {
+            return collect($value)->map(function ($item) use ($search, $replace) {
+                if (isset($item['text'])) {
+                    $item['text'] = str_replace($search, $replace, $item['text']);
+                }
+                if (isset($item['content']) && is_array($item['content'])) {
+                    $item['content'] = self::replaceSoftHyphens($item['content'], $search, $replace);
+                }
+                return $item;
+            })->all();
+        }
+        return $value;
     }
 }
